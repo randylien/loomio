d3a2d74317ad783a86b2356dcfa57445e1e7cb82
James Kiesel <james.kiesel@gmail.com>
2014-03-14 15:33:06 +1300

    Add translation caching & support for translating Discussion, Motion, and Vote
    Conflicts (resolved):
    	Gemfile
    	Gemfile.lock
    	app/controllers/comments_controller.rb
    	app/models/translation.rb
    	app/models/user.rb
    	app/models/vote.rb
    	app/views/comments/_comment_likes.html.haml
    	app/views/discussions/show.html.haml
    	app/views/motions/_motion.html.haml
    	app/views/motions/_votes_table_vote.html.haml
    	db/schema.rb
    	spec/models/ability_spec.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < BaseController
   load_and_authorize_resource only: :destroy
-  load_resource only: [:like, :translate]
-  skip_before_filter :authenticate_user!, only: :translate
+  load_resource only: [:like]
 
   def destroy
     CommentDeleter.new(@comment).delete_comment
@@ -20,10 +19,6 @@ class CommentsController < BaseController
     @discussion = @comment.discussion
 
     render :template => "comments/comment_likes"
-  end
-  
-  def translate
-    raise NotImplementedError # (temporarily disable translation feature) 
   end
 
 end
--- extras/app_translation.rb
+++ extras/app_translation.rb
@@ -1,4 +1,4 @@
-class Translation
+class AppTranslation
   LANGUAGES = {'English' => :en,
                'български' => :bg,
                'Català' => :ca,
@@ -26,7 +26,7 @@ class Translation
   FRONTPAGE_SUPPORTED_LOCALES = [:en, :pt, :el, :es, :ca, :cs, :fr, :uk, :nl]
 
   VIDEO_SUPPORTED_LOCALES  = [:en, :pt, :el, :es, :ca, :cs, :fr, :ja, :nl, :vi]
-
+  
   def self.language(locale)
     LANGUAGES.key(locale.to_sym)
   end
@@ -61,4 +61,5 @@ class Translation
       title: "#{I18n.t(:change_language, language: language[0])}",
       text: "#{language[0]}" }
   end
+
 end
